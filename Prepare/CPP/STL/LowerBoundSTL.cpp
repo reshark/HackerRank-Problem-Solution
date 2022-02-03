d068e6acdec7b56b87095d6a1451adbc62e20283
reshark <35295974+reshark@users.noreply.github.com>
2022-02-03 19:48:28 +0900

    add Prepare/CPP/STL/LowerBoundSTL

---
+++ Prepare/CPP/STL/LowerBoundSTL.cpp
@@ -0,0 +1,45 @@
+﻿#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+
+    //input N
+    int n;
+    cin >> n;
+
+    //input SortedOrder
+    vector<int> vecSortedOrder;
+    for (int i = 0; i < n; i++)
+    {
+        int nInput(0);
+        cin >> nInput;
+        vecSortedOrder.push_back(nInput);
+    }
+
+    //input Total Query
+    int nTotalQ;
+    cin >> nTotalQ;
+
+    for (int i = 0; i < nTotalQ; i++)
+    {
+        //input Query
+        int q(0);
+        cin >> q;
+
+        // use Lower Bound
+        vector<int>::iterator itlowerBound;
+        itlowerBound = lower_bound(vecSortedOrder.begin(), vecSortedOrder.end(), q);
+
+        if (vecSortedOrder[itlowerBound - vecSortedOrder.begin()] == q)
+            cout << "Yes" << " " << itlowerBound - vecSortedOrder.begin() + 1 << endl;
+        else
+            cout << "No" << " " << itlowerBound - vecSortedOrder.begin() + 1 << endl;
+    }
+
+    return 0;
+}
